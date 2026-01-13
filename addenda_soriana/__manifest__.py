491cc31dd83eca10e8e9a387e3afe44a2da3c6b2
BrunoHV <bruno_hv1@tesch.edu.mx>
2026-01-13 13:45:17 -0600

    Add __manifest__.py for Addenda Soriana module

---
+++ addenda_soriana/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Addenda Soriana',
+    'version': '1.0',
+    'category': 'Accounting',
+    'author': 'Bruno',
+    'depends': ['account', 'l10n_mx_edi', 'sale'],
+    'data': [
+        'views_form.xml',
+        'templates.xml',
+    ],
+    'installable': True,
+}
